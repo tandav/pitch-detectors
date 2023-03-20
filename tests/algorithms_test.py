ca1bc5826131b94474ab727a0f7b621d4109b687
Alexander Rodionov <tandav@tandav.me>
2023-03-20 10:33:08 +0300

    fix deprecation pkg_resources warnings

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -24,7 +24,8 @@ def record(rescale: float = 100000):
     return Record(a, fs)
 
 
-@pytest.mark.filterwarnings('ignore:pkg_resources')
+@pytest.mark.filterwarnings('ignore:pkg_resources is deprecated as an API')
+@pytest.mark.filterwarnings('ignore:Deprecated call to `pkg_resources.declare_namespace')
 @pytest.mark.parametrize('algorithm', ALGORITHMS)
 def test_detection(algorithm, record):
     algorithm(record.a, record.fs)

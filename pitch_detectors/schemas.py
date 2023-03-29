0b1854eeadc9586f29b2733dc9c5cb823b69d720
Alexander Rodionov <tandav@tandav.me>
2023-03-29 18:26:19 +0300

    Revert "make schemas.Record hashable"
    This reverts commit 64a3d89443596fd843aea9cbc3b3db1dd493b18b.

--- pitch_detectors/schemas.py
+++ pitch_detectors/schemas.py
@@ -21,11 +21,3 @@ class Record:
     a: np.ndarray | None = None
     t: np.ndarray | None = None
     f0: np.ndarray | None = None
-
-    def __hash__(self) -> int:
-        return hash((
-            self.fs,
-            self.a.tobytes() if self.a is not None else None,
-            self.t.tobytes() if self.t is not None else None,
-            self.f0.tobytes() if self.f0 is not None else None,
-        ))

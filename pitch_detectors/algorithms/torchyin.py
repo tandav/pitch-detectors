9ec3b678dc1461e04a828a1314bf18035c71758b
Alexander Rodionov <tandav@tandav.me>
2023-03-29 17:01:17 +0300

    convert TorchYin f0 tensor to numpy

--- pitch_detectors/algorithms/torchyin.py
+++ pitch_detectors/algorithms/torchyin.py
@@ -18,7 +18,7 @@ class TorchYin(PitchDetector):
         import torchyin
         super().__init__(a, fs)
         _a = torch.from_numpy(a)
-        f0 = torchyin.estimate(_a, sample_rate=self.fs, pitch_min=hz_min, pitch_max=hz_max)
+        f0 = torchyin.estimate(_a, sample_rate=self.fs, pitch_min=hz_min, pitch_max=hz_max).numpy()
         f0[f0 == 0] = np.nan
         self.f0 = f0[:-1]
         self.t = np.linspace(0, self.seconds, f0.shape[0])[1:]

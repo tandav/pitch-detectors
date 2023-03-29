9ffb71f13f20ccd4aab155f0af8b2995108a2605
Alexander Rodionov <tandav@tandav.me>
2023-03-29 18:03:41 +0300

    assert t and f0 shapes equal in algorithms_test

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -6,4 +6,5 @@ from pitch_detectors.algorithms import ALGORITHMS
 @pytest.mark.order(3)
 @pytest.mark.parametrize('algorithm', ALGORITHMS)
 def test_detection(algorithm, record):
-    algorithm(record.a, record.fs)
+    alg = algorithm(record.a, record.fs)
+    assert alg.f0.shape == alg.t.shape

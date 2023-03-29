f36e436e8cd231e3938fd30a628e5ceb68de9800
Alexander Rodionov <tandav@tandav.me>
2023-03-29 16:46:47 +0300

    move pytest filterwarnings to pyproject.toml

--- tests/algorithms_test.py
+++ tests/algorithms_test.py
@@ -4,9 +4,6 @@ from pitch_detectors.algorithms import ALGORITHMS
 
 
 @pytest.mark.order(3)
-@pytest.mark.filterwarnings('ignore:pkg_resources is deprecated as an API')
-@pytest.mark.filterwarnings('ignore:Deprecated call to `pkg_resources.declare_namespace')
-@pytest.mark.filterwarnings('ignore:distutils Version classes are deprecated')
 @pytest.mark.parametrize('algorithm', ALGORITHMS)
 def test_detection(algorithm, record):
     algorithm(record.a, record.fs)
